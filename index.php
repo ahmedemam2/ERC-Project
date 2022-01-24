18b99c839a76e2b2abf4fb61774ea7b02adad387
ahmedemam2 <88343933+ahmedemam2@users.noreply.github.com>
2022-01-25 01:37:19 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,73 +1,85 @@
-
-        <?php
-        $executing;
-        include_once 'TypeA.php';
-        include_once 'TypeAB.php';
-        include_once 'TypeB.php';
-        include_once 'TypeO.php';
-        include_once 'StateInterface.php';
-        include_once 'Executor.php'; 
-        include_once 'StateContext.php';       
-        $executing = new StateContext();
-        $check = $executing->setState("TypeA");
-
-        ?>
-<head>
+ <?php
+        include_once("config.php");
+ ?>
+<html>
+    <head>
         <meta charset="UTF-8">
-        <title>Create Record</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-        <style type="text/css">
-            body {
-            background-image: url("2.png");
-            }
-            .wrapper{
-                background-color: white;
-                opacity: 0.8;
-                width: 500px;
-                margin: 0 auto;
-            }
- 
-        </style>
+        <title></title>
     </head>
     <body>
-        <div class="wrapper">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="page-header">
-                            <h2 >Donate Blood</h2>
-                        </div>
-                        <p>Please fill this form to apply for our blood donation programme.</p>
-                        <form action="CreateController.php" method="post">
-                            <div class="form-group <?php echo (!empty($name_err)) ? 'has-error' : ''; ?>">
-                                <label>Name</label>
-                                <input type="text" name="name" class="form-control" value="">
-                                <span class="help-block"></span>
-                            </div>
-                              <div class="form-group <?php echo (!empty($desc_err)) ? 'has-error' : ''; ?>">
-                                 <form action="/action_page.php">
-                                 <p>Please select your favorite Web language:</p>
-                                 <input type="radio" id="Type_A" name="bloodtype" value="TYPEA">
-                                 <label for="typea">TypeA</label><br>
-                                 <input type="radio" id="Type_B" name="bloodtype" value="TYPEB">
-                                 <label for="typeb">TypeB</label><br>
-                                 <input type="radio" id="Type_AB" name="bloodtype" value="TYPEAB">
-                               <label for="typeab">TypeAB</label><br>
-                                 <input type="radio" id="Type_O" name="bloodtype" value="TYPEO">
-                                 <label for="typeo">TypeO</label>
+       
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Excel Demo</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  
+</head>
+<body>
 
-                                   <br>  
-
-                            </div>
-                            <input type="submit" class="btn btn-danger" value="Submit">
-                            <a href="../index.php" class="btn btn-default">Cancel</a>
-                        </form>
-                    </div>
-                </div>        
+<div class="container-fluid">
+    <div class="card">
+        <div class ="card-header text-center">
+            <h4>Report</h4>
+            <form action="process.php" method="post">
+                <button type="submit" name="submit" class="btn btn-primary float-right">Download</button>
+            </form>
+            <div class="card-body">
+                <table class="table table-striped table-hover">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">id</th>
+      <th scope="col">address</th>
+      <th scope="col">gender</th>
+      <th scope="col">age</th>
+      <th scope="col">Name</th>
+    </tr>
+  </thead>
+  <tbody>
+      
+      
+      <?php
+      $sql="SELECT * FROM testingg";
+      $res=mysqli_query($link, $sql);
+      if(mysqli_num_rows($res)>0)
+      {
+          $i=1; 
+          while($row=mysqli_fetch_array($res))
+          {
+              ?>
+               <tr>
+                  <th scope="row"><?php echo $i++; ?></th>
+                  <td><?php echo $row['id']?></td>
+                  <td><?php echo $row['address']?></td>
+                  <td><?php echo $row['gender']?></td>
+                  <td><?php echo $row['age']?></td>
+                  <td><?php echo $row['Name']?></td>
+                  <td></td>
+                  <td></td>
+               </tr>
+              <?php
+          }
+      } else {
+          echo("no data found");
+      }
+      
+      ?>
+   
+    
+  </tbody>
+</table>
             </div>
         </div>
-    </body>
-    
-        <?php
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html> 
         
-   
+    </body>
+</html>
